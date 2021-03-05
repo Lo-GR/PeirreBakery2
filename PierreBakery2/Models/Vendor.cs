2ce56f74c6c3512fa66f58cd5077498d8f96b0cd
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 12:57:31 -0800

    pass good fail

--- PierreBakery2/Models/Vendor.cs
+++ PierreBakery2/Models/Vendor.cs
@@ -6,9 +6,11 @@ namespace PierreBakery2.Models
   public class Vendor
   {
     public string Name {get;}
+    public int Id {get;}
     public Vendor(string vendorName)
     {
       Name = vendorName;
+      Id = 200;
     }
   }
 }
