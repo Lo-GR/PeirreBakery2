3404fb464606cbce9743123bfdc3e6a47f71d61b
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 10:04:48 -0800

    pass good fail

--- PierreBakery2/Models/Order.cs
+++ PierreBakery2/Models/Order.cs
@@ -5,9 +5,11 @@ namespace PierreBakery2.Models
 {
   public class Order
   {
+    public string Title {get; }
+
     public Order(string title, string description, int price, int year, int month, int day)
     {
-      
+      Title = "test";
     }
   }
 }
