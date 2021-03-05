3efa959fb05e718db142f23b0d04dd6a58175e7f
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 12:36:01 -0800

    write test for Vendor Constructor

--- PierreBakery2.Tests/ModelTests/VendorTests.cs
+++ PierreBakery2.Tests/ModelTests/VendorTests.cs
@@ -2,3 +2,17 @@
 using Pierrebakery2.Models;
 using System.Collections.Generic;
 using System;
+
+namespace PierreBakery2.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVen = new Vendor()
+      Assert.AreEqual(typeof(Vendor), newVen.GetType());
+    }
+  }
+}
