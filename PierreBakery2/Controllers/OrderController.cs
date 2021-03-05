17027e237d54c19a13835f28d58f9ed05614acc2
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 15:36:52 -0800

    add Order Controller

---
+++ PierreBakery2/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using PierreBakery2.Models;
+using System.Collections.Generic;
+
+namespace PierreBakery2.Controllers
+{
+  public class OrdersController : Controller
+  {
+    
+  }
+}
