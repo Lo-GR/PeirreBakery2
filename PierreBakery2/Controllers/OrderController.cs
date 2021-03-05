fd6d41ae78b40be891f320a654491e39cf299ae8
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 15:38:40 -0800

    create GET route for New Order

--- PierreBakery2/Controllers/OrderController.cs
+++ PierreBakery2/Controllers/OrderController.cs
@@ -6,6 +6,11 @@ namespace PierreBakery2.Controllers
 {
   public class OrdersController : Controller
   {
-    
+    [HttpGet("/vendors/{vendorId}/vendors/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
   }
 }
