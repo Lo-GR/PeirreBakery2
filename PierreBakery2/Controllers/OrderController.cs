d67dfe3e31c4ad6917c2ea29e7b23c88ad9ffb3b
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 15:42:03 -0800

    create GET route for Show Order

--- PierreBakery2/Controllers/OrderController.cs
+++ PierreBakery2/Controllers/OrderController.cs
@@ -12,5 +12,15 @@ namespace PierreBakery2.Controllers
       Vendor vendor = Vendor.Find(vendorId);
       return View(vendor);
     }
+    [HttpGet("/vendors/{vendorId]/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> modelToPass = new Dictionary<string, object>();
+      modelToPass.Add("order", order);
+      modelToPass.Add("vendor", vendor);
+      return View(modelToPass);
+    }
   }
 }
