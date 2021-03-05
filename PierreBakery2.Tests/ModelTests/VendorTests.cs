78d8e6623c6efdea17c2ad959faa7c6090209f0c
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 12:47:42 -0800

    write test for getting name of vendor

--- PierreBakery2.Tests/ModelTests/VendorTests.cs
+++ PierreBakery2.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,13 @@ namespace PierreBakery2.Tests
       Vendor newVen = new Vendor("Big Mac Daniel's Macaronis");
       Assert.AreEqual(typeof(Vendor), newVen.GetType());
     }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Big Mac Daniel's Macaronis";
+      Vendor newVen = new Vendor(name);
+      string result = newVen.Name;
+      Assert.AreEqual(name, result);
+    }
   }
 }
