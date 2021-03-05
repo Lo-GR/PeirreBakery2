ef27349189b49fa45b4ce6f41a40715b8533f3ad
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 13:20:30 -0800

    pass test for finding vendor by id

--- PierreBakery2/Models/Vendor.cs
+++ PierreBakery2/Models/Vendor.cs
@@ -24,7 +24,7 @@ namespace PierreBakery2.Models
     }
     public static Vendor Find(int searchId)
     {
-      return _instances[searchId - 2];
+      return _instances[searchId - 1];
     }
   }
 }
