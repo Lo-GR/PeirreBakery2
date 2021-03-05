5bf4b6db9c21c5f0389383690ab6869654c9ffb9
Lo-GR <diamondintheroth@gmail.com>
2021-03-05 12:45:30 -0800

    pass test for Vendor constructor

--- PierreBakery2.Tests/ModelTests/VendorTests.cs
+++ PierreBakery2.Tests/ModelTests/VendorTests.cs
@@ -12,7 +12,7 @@ namespace PierreBakery2.Tests
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVen = new Vendor("Big Mac Daniel's Macaronis");
-      Assert.AreEqual(typeof(Order), newVen.GetType());
+      Assert.AreEqual(typeof(Vendor), newVen.GetType());
     }
   }
 }
